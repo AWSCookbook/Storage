6c1c01879c530b9a4879bde1d9e0d9ab1cbd92a7
Mike Zazon <mzazon@gmail.com>
2021-10-26 22:26:19 -0400

    Updated READMEs

--- 305-Configuring-Application-Specific-Access-to-S3/cdk-AWS-Cookbook-305/cdk_aws_cookbook_305/cdk_aws_cookbook_305_stack.py
+++ 305-Configuring-Application-Specific-Access-to-S3/cdk-AWS-Cookbook-305/cdk_aws_cookbook_305/cdk_aws_cookbook_305_stack.py
@@ -91,7 +91,7 @@ class CdkAwsCookbook305Stack(Stack):
 
         iam_role1 = iam.Role(self, "Instance1SSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
 
-        iam_role1.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"))
+        iam_role1.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
 
         instance1 = ec2.Instance(
             self,
@@ -104,7 +104,11 @@ class CdkAwsCookbook305Stack(Stack):
 
         iam_role2 = iam.Role(self, "Instance2SSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
 
+<<<<<<< HEAD
         iam_role2.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"))
+=======
+        iam_role2.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+>>>>>>> d0119e3 (Updated READMEs)
 
         instance2 = ec2.Instance(
             self,
